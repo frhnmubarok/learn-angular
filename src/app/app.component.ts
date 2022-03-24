f0d5e62b776bfa95b9682a9b8436dda284caffda
frhnmubarok <farhanmubarok104@gmail.com>
2022-03-24 17:50:28 +0700

    learn structural & attribute directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'team-app';
+  newMemberName: string = '';
+  members: string[] = [];
+  errorMsg: string = '';
+
+  addMember() {
+    if (!this.newMemberName) {
+      this.errorMsg = 'Please enter a name';
+    } else {
+      this.members.push(this.newMemberName);
+      this.newMemberName = '';
+    }
+  }
+
+  onInput(member: string) {
+    this.newMemberName = member;
+    console.log(this.newMemberName);
+  }
 }
